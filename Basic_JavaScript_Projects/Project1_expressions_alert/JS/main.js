666b42d14c36f4fcd2475b1efb1088f904040337
Ricardo  Lucero <96747085+RicardoLucero@users.noreply.github.com>
2022-02-03 13:29:15 -0700

    created Basic JavaScript folder
    created a basic html file and created JaveScript file

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,32 @@
+window.alert("Hello,World!");// created a window alert
+
+document.write('Hello, World!');//wrote out hello, world
+
+var R = "Ricardo Lucero";//assigned variable to string
+
+document.write(R);
+
+var myDog = "Mordecai";
+
+window.alert(myDog);
+
+document.write("Jasmine asked Ricardo \"will you take out the trash? \" \" of course I can\" Ricardo replied.");
+
+document.write("The student said \"my dog ate my homework\"" + "the teacher responded with" + "\"How did your dog eat your laptop?\"");
+
+var J = "Jasmine" + "Cardenas";//concatanated two strings
+
+document.write(J);
+
+var M = "Mordecai", C = "Claudius", P = "Pearl", S = "Summer";//assigned multple variables at the same time
+
+document.write(M);
+
+var X = "5 + 5";
+
+document.write(X);
+
+var text = "Flamingo";
+var result = text.fontcolor("pink");//assigned color to variable text
+
+document.write(result);
